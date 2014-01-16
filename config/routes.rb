9889d91d538cd6670dcbbc99c290d69af9d44a02
hvasdev <harvinder@connamara.com>
2014-01-16 08:33:25 +0530

    Added welcome controller/view and configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Angular::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: 'welcome#index'
+  get 'welcome/index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
