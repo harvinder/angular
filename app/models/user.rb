e5f399a1eca4b711ecb5419406421ed10e4507c4
hvasdev <harvinder@connamara.com>
2014-01-21 05:19:30 +0530

    Updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation
+  validates_presence_of :first_name, :last_name, :on => :create
+  validates_uniqueness_of :email
   has_secure_password
 end
