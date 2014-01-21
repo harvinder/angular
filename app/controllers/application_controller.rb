8a5e195f31bb1fa261cda8275f744c2293ea1a4c
hvasdev <harvinder@connamara.com>
2014-01-21 07:11:27 +0530

    Added current_user method available to all views and controllers to access the currently logged in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  private 
+  
+  def current_user
+    unless @current_user
+      user = nil
+      user = User.find( session[ :user_id ] ) if session[ :user_id ]
+      @current_user ||= user
+    end
+    @current_user
+  end
+  
+  helper_method :current_user
 end
